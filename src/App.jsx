772fc3c4974b7f6435fc5379a92ba15d79dddf38
Wayne Stedman <waynestedman@users.noreply.github.com>
2025-09-19 17:27:40 -0700

    started stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // App.jsx
+import Stuff from './Stuff';
 import Card from './Card';
 
 function App() {
@@ -9,17 +10,28 @@ function App() {
         <h3>CSS Animation</h3>
       </header>
       <main>
-        <Card 
-          title="Hover over this Card!"
-          body="This is a card. Hover over me to see the animation effect."
-          imageUrl="https://picsum.photos/300/200"
-        />
-        <Card 
-          additionalClassName="second-card"
-          title="Another Card"
-          body="This is another card. These are Millie & Neo."
-          imageUrl="https://placecats.com/millie_neo/300/200"
-        />
+        <section className='moving-stuff'>
+          <Stuff />
+        </section>
+        <section className='card-container'>
+          <Card 
+            title="Hover over this Card!"
+            body="This is a card. Hover over me to see the animation effect."
+            imageUrl="https://picsum.photos/300/200?grayscale"
+          />
+          <Card 
+            additionalClassName="second-card"
+            title="Another Card"
+            body="This is another card. These are Millie & Neo."
+            imageUrl="https://placecats.com/millie_neo/300/200"
+          />
+          <Card 
+            additionalClassName="third-card"
+            title="Ummm ... Dog?"
+            body="Dogs are cool! Right?"
+            imageUrl="https://placedog.net/300/200?id=1"
+          />
+        </section>
       </main>
     </>
   )
---
+++ src/Stuff.jsx
@@ -0,0 +1,16 @@
+// Stuff.jsx
+import '../src/css/stuff.css';
+
+function Stuff() {
+  return (
+    <>
+      <div className='box box1'></div>
+      <div className='box box2'></div>
+      <div className='box box3'></div>
+      <div className='box box4'></div>
+      <div className='box box5'></div>
+    </>
+  );
+}
+
+export default Stuff;
